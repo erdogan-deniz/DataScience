c35c2b7a66293cd891bbc9dcd973e6d9ee5939f6
Deniz <68436196+Denzi33@users.noreply.github.com>
2024-06-14 01:41:57 +0300

    Update main.py

--- Machine learning algorithms/main.py
+++ Machine learning algorithms/main.py
@@ -1,1 +1,17 @@
+"""
+------------------------------------------------------------------------------------------------------------------------
+Module description:
+    Module is used to work with machine learning models.
+    It's similar with module scikit-learn.
+------------------------------------------------------------------------------------------------------------------------
+Classes include:
+    LinearRegression: A class of linear regression.
+------------------------------------------------------------------------------------------------------------------------
+Additional information:
+    Last update: 13.06.2024.
+    Author FN: Deniz Erdogan.
+    Contact: erdogan33@mail.ru.
+------------------------------------------------------------------------------------------------------------------------
+"""
 
+import linear_regression
