d3037c4ac64ad13a26a2a78a86f9967b27cd7b5b
Deniz <68436196+Denzi33@users.noreply.github.com>
2024-06-16 02:40:22 +0300

    Update main.py

--- Machine learning algorithms/main.py
+++ Machine learning algorithms/main.py
@@ -8,7 +8,7 @@ Classes include:
     LinearRegression: A class of linear regression.
 ------------------------------------------------------------------------------------------------------------------------
 Additional information:
-    Last update: 13.06.2024.
+    Last update: 16.06.2024.
     Author FN: Deniz Erdogan.
     Contact: erdogan33@mail.ru.
 ------------------------------------------------------------------------------------------------------------------------
